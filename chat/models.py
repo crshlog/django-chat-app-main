2da810ce2facbfa66e0e0fe59662dbc5277b2fc9
crshlog <144978755+crshlog@users.noreply.github.com>
2023-09-27 01:28:31 +0700

    i dont

--- chat/models.py
+++ chat/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from datetime import datetime
 
+current_date = datetime.now()
+formatted_date = current_date.strftime("%m-%d")
+print(formatted_date)
+
 # Create your models here.
 class Room(models.Model):
     name = models.CharField(max_length=1000)
--- chat/views.py
+++ chat/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
 from chat.models import Room, Message
 from django.http import HttpResponse, JsonResponse
+import random
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def room(request, room):
-    username = request.GET.get('username')
+    username = 'a certain someone'
     room_details = Room.objects.get(name=room)
     return render(request, 'room.html', {
         'username': username,
@@ -17,7 +18,7 @@ def room(request, room):
 
 def checkview(request):
     room = request.POST['room_name']
-    username = request.POST['username']
+    username = 'a certain someone'
 
     if Room.objects.filter(name=room).exists():
         return redirect('/'+room+'/?username='+username)
